4abb726f097cd6969bf7ae20eea9d9df9037f59d
josemage16 <42522036+josemage16@users.noreply.github.com>
2024-01-10 13:33:53 -0300

    Update JGEs.sol

--- JGEs.sol
+++ JGEs.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract JGEs {
-    string public storedText;
-
-    function saveText(string memory newText) public {
-        storedText = newText;
-    }
+    string public storedText = "8f10646445eed0e0e486bd2a975957079a60e016aeacda53394f51dc3b88a706";
+    
+   
 }
