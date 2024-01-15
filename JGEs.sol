1fa4b5cefdcd30725a306715f539676adba56166
josemage16 <42522036+josemage16@users.noreply.github.com>
2024-01-15 11:27:58 -0300

    Update JGEs.sol

--- JGEs.sol
+++ JGEs.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract JGEs {
-    string public storedText = "8f10646445eed0e0e486bd2a975957079a60e016aeacda53394f51dc3b88a706";
-    
-   
-}
+contract JGE_2 {
+    // State variable to store the string
+    string private storedString = "8f10646445eed0e0e486bd2a975957079a60e016aeacda53394f51dc3b88a706";
+
+    // Function to set the string
+    //function setString(string memory _newString) public {
+        //storedString = _newString;
+   // }
+
+    // Function to get the stored string
+    function getString() public view returns (string memory) {
+        return storedString;
